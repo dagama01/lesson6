ef364f3bf2a6e93df3b57a031a0f7c3898fd6c19
Kanat <abdsattar02@inbox.ru>
2021-11-04 22:16:38 +0600

    Task is done

---
+++ src/main/java/kz/gb/kanat/Animal.java
@@ -0,0 +1,17 @@
+package kz.gb.kanat;
+
+public class Animal {
+    String name;
+    int run;
+
+    public Animal() {
+
+    }
+    public Animal(String name){
+        this.name = name;
+    }
+    public void animalInfo() {
+        System.out.println();
+
+    }
+}
